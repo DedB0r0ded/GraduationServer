ad396a5f78a5d4fc56b383b6ceb48b817d1733b3
root <root@2866872-xb46324.twc1.net>
2024-06-05 23:52:01 +0300

    Associations

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,5 +4,6 @@ class Service < ApplicationRecord
   belongs_to :default_item, class_name: "Item"
   has_many :organisation_services
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 64 }
+  validates :description, length: { maximum: 512 }
 end
