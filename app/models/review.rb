cbdfd1e64794e4e72a58f9e6be7fbc42121e2fe2
DedB0r0ded <revikad.00@mail.ru>
2024-06-06 16:16:21 +0300

    Minor changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
+  belongs_to :user
+  belongs_to :facility
+
+  validates :app_created_at, :subject, :user_id, :facility_id, presence: true
+  validates :subject, length: { maximum: 64 }
+  validates :body, length: { minimum: 10, maximum: 512 }
 end
